3635c19f3a51222ca1e4b063dac5d8b1103ecb7e
maryem <maryyemhanyy@gmail.com>
2026-05-24 00:38:10 +0300

    SessionModel

---
+++ GymSystem.BLL/Services/Classes/SessionService.cs
@@ -0,0 +1,55 @@
+﻿using GymSystem.BLL.Common;
+using GymSystem.BLL.Services.Interfaces;
+using GymSystem.BLL.ViewModels.SessionViewModels;
+using GymSystem.DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymSystem.BLL.Services.Classes
+{
+    public class SessionService(IUnitOfWork unitOfWork) : ISessionService
+    {
+        public Task<Result> CreateSessionAsync(CreateSessionViewModel model, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result> DeleteSessionAsync(int id, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<SessionViewModel>?> GetAllSessionsAsync(CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<CategorySelectViewModel>> GetCategoriesForSelectAsync(CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SessionViewModel?> GetSessionByIdAsync(int id, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UpdateSessionViewModel> GetSessionForUpdateAsync(int id, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TrainerSelectViewModel>> GetTrainersForSelectAsync(CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result> UpdateSessionAsync(int id, UpdateSessionViewModel model, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GymSystem.DAL/Repositories/Classes/SessionRepository.cs
@@ -0,0 +1,42 @@
+﻿using GymSystem.DAL.DbContexts;
+using GymSystem.DAL.Models;
+using GymSystem.DAL.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymSystem.DAL.Repositories.Classes
+{
+    public class SessionRepository : GenericRepository<Session>, ISessionRepository
+    {
+        private readonly GymDbContext _dbContext;
+
+        public SessionRepository(GymDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<IEnumerable<Session>> GetAllSessionsWithTrainerAndCategoryAsync(Expression<Func<Session, bool>>? predicate = null, CancellationToken ct = default)
+        {
+            IQueryable<Session> query = _dbContext.Sessions.Where(predicate);
+
+            if (predicate != null)
+            {
+                query = query.Where(predicate);
+            }
+
+            return await query.ToListAsync();
+        }
+
+        public Task<int> GetCountOfBookedSlotsAsync(int id, CancellationToken ct = default)
+            =>_dbContext.Bookings.AsNoTracking().CountAsync(b => b.SessionId == id, ct);
+        
+
+        public Task<Session?> GetSessionWithTrainerAndCategoryAsync(int id, CancellationToken ct = default)
+         => _dbContext.Sessions.AsNoTracking().Include(s => s.Category).FirstOrDefaultAsync(s => s.Id == id, ct);
+        
+    }
+}
